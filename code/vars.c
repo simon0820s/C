b1c57746714c9c3caee8956ee372172a52d587fa
simon0820s <simon0820s@gmail.com>
2023-01-28 23:24:07 -0500

    introduccion terminada

---
+++ code/vars.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    //declarando variables
+    unsigned int n1 = 20;
+    unsigned int n2 = 30;
+    //haciendo suma
+    int suma=n1+n2;
+    char nl='\n';
+    //mostrando la suma
+    printf("la suma es = %d",suma);
+    //declarar constante
+    printf("%c",nl);
+    const int xp=30;
+    printf("la experiencia es: %d",xp);
+    printf("%c",nl);
+    return 0;
+}
