9c8640d6cfb926b798621f5207baba86d5180d2a
simon0820s <simon0820s@gmail.com>
2023-01-29 11:32:31 -0500

    for

---
+++ basic/while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int i = 1;
+    while(i<10){
+        printf("%d \n",i);
+        i=i+1;
+    }
+
+    do
+    {
+        printf("%d \n",i);
+        i=i-1;
+    } while (i>=1);
+    
+    
+    return 0;
+}
